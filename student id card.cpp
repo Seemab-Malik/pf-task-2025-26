ccf739e684db66fb9d2962235d2108599f699a7f
Seemab-Malik <seemabmalik355@gmail.com>
2026-01-13 09:26:29 +0500

    Update student ID card details

--- student id card.cpp
+++ student id card.cpp
@@ -5,11 +5,12 @@ int main()
 	cout<<"----------------------------------------------------\n";
 	cout<< "\tstudent ID Card\n";
 	cout<<"----------------------------------------------------\n";
-	cout<< "Name:\t\tshahzaib\n";
-	cout<< "Roll No:\t45\n";
+	cout<< "Name:\t\tseemab malik\n";
+	cout<< "Roll No:\t26\n";
 	cout<< "Deperment:\tcomputer science\n";
 	cout<< "Session:\t2025-2029\n";
 	cout<<"----------------------------------------------------\n";
 	return 0;
 	
+
 }
